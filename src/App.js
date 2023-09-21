939e40f974a9fcb0ac73dad80d4601be4e9c541c
kushal-rajkarnikar <kushal.rajkarnikar@gmail.com>
2023-09-21 14:06:56 +0545

    push news to git

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Content from "./components/Content";
+import React from "react";
 
 function App() {
+  const API_KEY = "0c5e171021c74198b852039622758663";
+
+  const [news, setNews] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+
+  const [searchQuery, setSearchQuery] = React.useState("");
+
+  const NewsArticles = [];
+
+  React.useEffect(() => {
+    setLoading(true);
+    fetch(
+      "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=" + API_KEY
+    )
+      .then((res) => res.json())
+      .then((data) => setNews(data.articles))
+      .then(setLoading(false));
+  }, []);
   return (
     <>
       <NavBar />
-      <Content />
+      <Content news={news} />
     </>
   );
 }
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,25 +1,27 @@
 import React from "react";
 import NewsBlock from "./NewsBlock";
 
-export default function Content() {
-  const API_KEY = "0c5e171021c74198b852039622758663";
+export default function Content(props) {
+  // const API_KEY = "0c5e171021c74198b852039622758663";
 
-  const [news, setNews] = React.useState([]);
-  const [loading, setLoading] = React.useState(false);
+  // const [news, setNews] = React.useState([]);
+  // const [loading, setLoading] = React.useState(false);
 
-  const NewsArticles = [];
+  // const[searchQuery, setSearchQuery] = React.useState("");
 
-  React.useEffect(() => {
-    setLoading(true);
-    fetch(
-      "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=" + API_KEY
-    )
-      .then((res) => res.json())
-      .then((data) => setNews(data.articles))
-      .then(setLoading(false));
-  }, []);
+  // const NewsArticles = [];
 
-  console.log(news);
+  // React.useEffect(() => {
+  //   setLoading(true);
+  //   fetch(
+  //     "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=" + API_KEY
+  //   )
+  //     .then((res) => res.json())
+  //     .then((data) => setNews(data.articles))
+  //     .then(setLoading(false));
+  // }, []);
+
+  // console.log(news);
 
   //   const NewsArticles = news.map(function (newsItem) {
   //     return <NewsBlock title={newsItem.title} />;
@@ -35,11 +37,14 @@ export default function Content() {
         </div>
 
         <div className="news-columns row">
-          {loading
-            ? "Loading..."
-            : news.map(function (newsItem) {
-                return <NewsBlock newsitem={newsItem} />;
-              })}
+          {
+            // loading
+            //   ? "Loading..."
+            //   :
+            props.news.map(function (newsItem) {
+              return <NewsBlock newsitem={newsItem} />;
+            })
+          }
         </div>
       </div>
     </main>
